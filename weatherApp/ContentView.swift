ca30a48deef6c67700df1d6a4fafe39ba710f51c
Aljwhra <aljwhra.n2@icloud.com>
2022-11-02 11:41:38 +0300

    Initial Commit

---
+++ weatherApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  weatherApp
+//
+//  Created by Aljwhra Alnasser on 08/04/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Aljwhra Alnasser on 08/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
